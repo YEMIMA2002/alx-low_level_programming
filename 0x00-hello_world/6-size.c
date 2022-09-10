1e615668292efebdb5692f714180a7ca592f6956
YEMIMA2002 <gbadabizoyemimahonorine@gmail.com>
2022-09-10 03:06:56 -0700

    First C Work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,11 +14,10 @@ int main(void)
 	long long Long_Long;
 	float a_float;
 
-	printf("Size of a char: %lu byte(s)\n", sizeof(Character));
-	printf("Size of an int: %lu byte(s)\n", sizeof(Integer));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(Long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(Long_Long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(a_float));
+	printf ("Size of a char: %lu byte(s)\n", sizeof(Character));
+	printf ("Size of an int: %lu byte(s)\n", sizeof(Integer));
+	printf ("Size of a long int: %lu byte(s)\n", sizeof(Long));
+	printf ("Size of a long long int: %lu byte(s)\n", sizeof(Long_Long));	        printf ("Size of a float: %lu byte(s)\n", sizeof(a_float));
 
 	return(0);
 }
