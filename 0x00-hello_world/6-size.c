ce4c8980fc503a5062565fca91a569dd8d31bb1e
YEMIMA2002 <gbadabizoyemimahonorine@gmail.com>
2022-09-10 03:32:53 -0700

    First C Work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /*
  * main - Entry point
  *
- * Return: 0 always (Success)
+ * Return: Always 0 (success)
  */
 
 int main(void)
@@ -14,10 +14,11 @@ int main(void)
 	long long Long_Long;
 	float a_float;
 
-	printf ("Size of a char: %lu byte(s)\n", sizeof(Character));
-	printf ("Size of an int: %lu byte(s)\n", sizeof(Integer));
-	printf ("Size of a long int: %lu byte(s)\n", sizeof(Long));
-	printf ("Size of a long long int: %lu byte(s)\n", sizeof(Long_Long));	        printf ("Size of a float: %lu byte(s)\n", sizeof(a_float));
+	printf("Size of a char: %lu byte(s)\n", sizeof(Character));
+	printf("Size of an int: %lu byte(s)\n", sizeof(Integer));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(Long));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(Long_Long));
+	printf("Size of a float: %lu byte(s)\n", sizeof(a_float));
 
 	return(0);
 }
