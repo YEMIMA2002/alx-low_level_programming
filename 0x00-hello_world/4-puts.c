64f5958139b58b9d5d289c357810e1f93a4ccae9
YEMIMA2002 <gbadabizoyemimahonorine@gmail.com>
2022-09-10 00:17:22 -0700

    First C Work

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main-printing
  *
- * Return: 0
+ * Return:Always 0 (succesful)
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
+
 }
