c464e0688c46e70be974826a96b16599edf1dc08
YEMIMA2002 <gbadabizoyemimahonorine@gmail.com>
2022-09-14 21:33:07 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * prints - alphabet in lowercase
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char;
+
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+
+}
